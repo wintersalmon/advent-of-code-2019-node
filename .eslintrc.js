a4096a6b5938e9d46ebe1f0bebb81858f0c3f310
Hungjoon <Hungjoon@publy.co>
2020-10-17 16:05:11 +0900

    Add tslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,51 @@
 module.exports = {
   env: {
-    browser: true,
     es6: true,
+    node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
+    project: 'tsconfig.eslint.json',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
   },
   rules: {
-    indent: ['error', 2],
-    'comma-dangle': ['error', 'always-multiline'],
-    'no-console': 0,
+    '@typescript-eslint/no-use-before-define': 'off',
+    'no-console': 'warn',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+      },
+    ],
+    'newline-before-return': 2,
+    'padding-line-between-statements': [
+      'warn',
+      { blankLine: 'always', prev: '*', next: 'block' },
+      { blankLine: 'always', prev: 'block', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'block-like' },
+      { blankLine: 'always', prev: 'block-like', next: '*' },
+    ],
+    'newline-after-var': ['error', 'always'],
   },
 };
